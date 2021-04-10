78024b51ad3f02e30b0cd5e01ffad76d2d08b7e7
Javier Ojeda <javier.ojeda@lucual18.es>
2021-04-10 18:07:47 +0200

    Adding new code

--- crud-nodejs-mongo/src/routes/index.js
+++ crud-nodejs-mongo/src/routes/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Task = require('../models/tasks')
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.render('index', {
+        tasks
+    });
+});
+
+
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.send('recived');
+});
 
 module.exports = router;
