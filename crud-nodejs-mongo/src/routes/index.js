01d6bfb982beb39db1637c7e634fe95270dd4f4f
Javier Ojeda <javier.ojeda@lucual18.es>
2021-04-11 03:31:36 +0200

    No work update functionality

--- crud-nodejs-mongo/src/routes/index.js
+++ crud-nodejs-mongo/src/routes/index.js
@@ -14,7 +14,35 @@ router.get('/', async (req, res) => {
 router.post('/add', async (req, res) => {
     const task = new Task(req.body);
     await task.save();
-    res.send('recived');
+    res.redirect('/');
+});
+
+router.get('/turn/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status
+    await task.save()
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('edit', {
+        task
+    });
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.updateOne({_id: id}, req.body);
+    res.redirect('/');
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.remove({_id: id});
+    res.redirect('/');
 });
 
 module.exports = router;
